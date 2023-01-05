448a21a3d34a68126aed8dc3247f72c09bb48cd1
Oakar Phyoe <67640388+Oakar-Phyo@users.noreply.github.com>
2023-01-05 09:22:22 +0630

    Update msg_timer_bot.py

--- msg_timer_bot.py
+++ msg_timer_bot.py
@@ -6,7 +6,7 @@ groupid= your groupid
 msg =['Hey Man','What Up!','Are you ok?']
 
 for send in msg:
-  url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={groupid}&text={msg}'
+  url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={groupid}&text={send}'
   requests.get(url)
   time.sleep(15)
   print (send)
