bb0e13ee9ba661875326cbd41e9701c3607686f3
Oakar Phyoe <67640388+Oakar-Phyo@users.noreply.github.com>
2023-01-05 09:30:39 +0630

    Update telg_bot.py

--- telg_bot.py
+++ telg_bot.py
@@ -7,13 +7,13 @@ raw_TS = datetime.now(ryan)
 curr_date = raw_TS.strftime("%d-%m-%Y")
 curr_time = raw_TS.strftime("%H:%M:%S")
 
-telg_token = "5982639798:AAGlm4LMOvt6zYb9cBuWRE11XRT5B-X_IOQ"
-telg_groupid = "-1001639787748"
+token = your token
+groupid =your groupid
 
 msg = f"Hey Man! {curr_date} at {curr_time} "
 
 def sendmgs_telg(message):
-    telg_api = f"https://api.telegram.org/bot{telg_token}/sendMessage?chat_id=@{telg_groupid}&text={message}"
+    telg_api = f"https://api.telegram.org/bot{token}/sendMessage?chat_id=@{groupid}&text={message}"
     tel_rep = requests.get(telg_api)
 
     if tel_rep.status_code == 200:
