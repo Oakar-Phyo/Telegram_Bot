a907d3dc1e544e993886f161d6e157e20dbbdf7f
Oakar Phyoe <67640388+Oakar-Phyo@users.noreply.github.com>
2023-01-05 09:41:03 +0630

    Update call_bot.py

--- call_bot.py
+++ call_bot.py
@@ -1,16 +1,17 @@
 import requests 
-# import time
-Token = '5982639798:AAGlm4LMOvt6zYb9cBuWRE11XRT5B-X_IOQ'
-groupid = "-1001639787748"
+
+# Token =your token
+# groupid = your groupid
+
 msg =['Hey Man! This is Assistant calling to you to inform you about something that is happening at your house']
 
 for send in msg:
-  call_url = 'https://api.callmebot.com/start.php?chat_id={}&text={}'.format(groupid ,send)
-  sms = f'https://api.callmebot.com/signal/send.php?chat_id={groupid}&apikey={Token}&text={send}'
-  call = 'http://api.callmebot.com/start.php?source=HA&chat_id=-1001639787748&text={}&lang=en-GB-Standard-B'.format(send)
-  url = 'https://api.telegram.org/bot5982639798:AAGlm4LMOvt6zYb9cBuWRE11XRT5B-X_IOQ/sendMessage?chat_id=-1001639787748&text={}'.format(send)
-  requests.get(sms)
+  
+#   call_url = f'https://api.callmebot.com/start.php?chat_id={groupid}&text={send}'
+#   sms = f'https://api.callmebot.com/signal/send.php?chat_id={groupid}&apikey={Token}&text={send}'
+
+  call = f'http://api.callmebot.com/start.php?source=HA&user=@oakarphyoe&text={send}&lang=en-GB-Standard-B'
+  requests.get(call)
   print(send)
 
-#   time.sleep(15)
  
